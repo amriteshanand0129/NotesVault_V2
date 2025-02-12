d5c0197f9abc5360b420fedf43e4263d264f7c55
AMRITESH ANAND <amritesh2901@gmail.com>
2025-02-12 16:51:53 +0530

    fix(depen): remove unwanted dependencies

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
+  server: { port: 3000 },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.split("node_modules/")[1].split("/")[0]; // Separate vendor chunks
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Increase chunk size warning limit (optional)
   },
-})
+});
