70cf028e0c4056d14c9e2e57ef4b91bed00ab915
AMRITESH ANAND <amritesh2901@gmail.com>
2025-03-30 19:12:41 +0530

    feat(nav): update navbar item for ADMIN

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -53,7 +53,7 @@ export default function Navbar() {
               <div className="flex space-x-4">
                 {navigation.map((item) => (
                   <Link key={item.name} to={item.href} aria-current={item.current ? "page" : undefined} className={classNames(item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white", "rounded-md px-3 py-2 text-sm font-medium")}>
-                    {item.name}
+                    {item.name === "Contribute" && user && user.userType === "ADMIN" ? "Add Resource" : item.name}
                   </Link>
                 ))}
               </div>
